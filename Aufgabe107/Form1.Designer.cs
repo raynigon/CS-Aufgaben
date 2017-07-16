768110d222a0b6e19fa6129ea2f09c5e1ef616a2
Simon Schneider <sim@raynigon.de>
2017-07-16 11:50:52 +0200

    Implemented Task 107

--- Aufgabe107/Form1.Designer.cs
+++ Aufgabe107/Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPhoneNumber = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbsName = new System.Windows.Forms.TextBox();
+            this.lblNameSearch = new System.Windows.Forms.Label();
+            this.tbsPhoneNumber = new System.Windows.Forms.TextBox();
+            this.lblPhoneSearch = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 15);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(12, 41);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(43, 13);
+            this.lblTelefon.TabIndex = 2;
+            this.lblTelefon.Text = "Telefon";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(99, 12);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(173, 20);
+            this.tbName.TabIndex = 3;
+            // 
+            // tbPhoneNumber
+            // 
+            this.tbPhoneNumber.Location = new System.Drawing.Point(99, 38);
+            this.tbPhoneNumber.Name = "tbPhoneNumber";
+            this.tbPhoneNumber.Size = new System.Drawing.Size(173, 20);
+            this.tbPhoneNumber.TabIndex = 4;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(197, 64);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.OnInsert);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(197, 165);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.OnSearch);
+            // 
+            // tbsName
+            // 
+            this.tbsName.Location = new System.Drawing.Point(99, 113);
+            this.tbsName.Name = "tbsName";
+            this.tbsName.Size = new System.Drawing.Size(173, 20);
+            this.tbsName.TabIndex = 9;
+            this.tbsName.TextChanged += new System.EventHandler(this.OnSearch);
+            // 
+            // lblNameSearch
+            // 
+            this.lblNameSearch.AutoSize = true;
+            this.lblNameSearch.Location = new System.Drawing.Point(12, 116);
+            this.lblNameSearch.Name = "lblNameSearch";
+            this.lblNameSearch.Size = new System.Drawing.Size(35, 13);
+            this.lblNameSearch.TabIndex = 10;
+            this.lblNameSearch.Text = "Name";
+            // 
+            // tbsPhoneNumber
+            // 
+            this.tbsPhoneNumber.Location = new System.Drawing.Point(99, 139);
+            this.tbsPhoneNumber.Name = "tbsPhoneNumber";
+            this.tbsPhoneNumber.ReadOnly = true;
+            this.tbsPhoneNumber.Size = new System.Drawing.Size(173, 20);
+            this.tbsPhoneNumber.TabIndex = 11;
+            // 
+            // lblPhoneSearch
+            // 
+            this.lblPhoneSearch.AutoSize = true;
+            this.lblPhoneSearch.Location = new System.Drawing.Point(12, 142);
+            this.lblPhoneSearch.Name = "lblPhoneSearch";
+            this.lblPhoneSearch.Size = new System.Drawing.Size(43, 13);
+            this.lblPhoneSearch.TabIndex = 12;
+            this.lblPhoneSearch.Text = "Telefon";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 199);
+            this.Controls.Add(this.lblPhoneSearch);
+            this.Controls.Add(this.tbsPhoneNumber);
+            this.Controls.Add(this.lblNameSearch);
+            this.Controls.Add(this.tbsName);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.tbPhoneNumber);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPhoneNumber;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbsName;
+        private System.Windows.Forms.Label lblNameSearch;
+        private System.Windows.Forms.TextBox tbsPhoneNumber;
+        private System.Windows.Forms.Label lblPhoneSearch;
     }
 }
 
--- Aufgabe107/Form1.cs
+++ Aufgabe107/Form1.cs
@@ -12,9 +12,37 @@ namespace Aufgabe107
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, string> contacts;
+
         public Form1()
         {
+            contacts = new Dictionary<string, string>();
             InitializeComponent();
         }
+
+        private void OnInsert(object sender, EventArgs e)
+        {
+            contacts.Add(tbName.Text, tbPhoneNumber.Text);
+            tbName.Clear();
+            tbPhoneNumber.Clear();
+            OnSearch(sender, e);
+        }
+
+        private void OnSearch(object sender, EventArgs e)
+        {
+            if (contacts.ContainsKey(tbsName.Text))
+            {
+                tbsPhoneNumber.Text = contacts[tbsName.Text];
+            }
+            else if (sender == btnSearch)
+            {
+                tbsPhoneNumber.Text = " - Not Found - ";
+            }
+            else
+            {
+                tbsPhoneNumber.Text = " - ";
+            }
+        }
+
     }
 }
