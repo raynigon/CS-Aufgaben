cfe04a62bd7f4e9415dbce84d45a099f13ea357b
Simon Schneider <sim@raynigon.de>
2017-07-16 11:00:48 +0200

    Implemented Task 105

--- Aufgabe105/Form1.Designer.cs
+++ Aufgabe105/Form1.Designer.cs
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.stackList = new System.Windows.Forms.ListBox();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // stackList
+            // 
+            this.stackList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.stackList.FormattingEnabled = true;
+            this.stackList.Location = new System.Drawing.Point(13, 13);
+            this.stackList.Name = "stackList";
+            this.stackList.Size = new System.Drawing.Size(120, 238);
+            this.stackList.TabIndex = 0;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput.Location = new System.Drawing.Point(140, 13);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(132, 20);
+            this.tbInput.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(140, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(60, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.OnMathAction);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMultiply.Location = new System.Drawing.Point(212, 39);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(61, 23);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.OnMathAction);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(139, 68);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(60, 23);
+            this.btnSubstract.TabIndex = 4;
+            this.btnSubstract.Text = "-";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.OnMathAction);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDivide.Location = new System.Drawing.Point(211, 68);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(61, 23);
+            this.btnDivide.TabIndex = 5;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.OnMathAction);
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(140, 98);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(132, 23);
+            this.btnPush.TabIndex = 6;
+            this.btnPush.Text = "Push";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(139, 127);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(132, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.stackList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox stackList;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- Aufgabe105/Form1.cs
+++ Aufgabe105/Form1.cs
@@ -16,5 +16,66 @@ namespace Aufgabe105
         {
             InitializeComponent();
         }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int value = int.Parse(tbInput.Text);
+                stackList.Items.Insert(0, value);
+            }
+            catch (FormatException exception)
+            {
+                tbInput.Text = "ERROR: Unable to parse Input";
+            }
+
+        }
+
+        private void OnMathAction(object sender, EventArgs e)
+        {
+            if (stackList.Items.Count < 2)
+            {
+                tbInput.Text = "ERROR: not enough Numbers in Stack";
+                return;
+            }
+
+            int b = PopItemFromStack();
+            int a = PopItemFromStack();
+            int c = 0;
+            if (sender == btnAdd)
+            {
+                c = a + b;
+            }else if (sender == btnSubstract)
+            {
+                c = a - b;
+            }
+            else if (sender == btnMultiply)
+            {
+                c = a * b;
+            }
+            else if (sender == btnDivide)
+            {
+                c = a / b;
+                tbInput.Text = "Rest: " + (a % b);
+            }
+
+            stackList.Items.Add(c);
+        }
+
+        private int PopItemFromStack()
+        {
+            if(stackList.Items.Count<1)
+                throw new FormatException("Empty Stack");
+            int x = ((int) stackList.Items[0]);
+            stackList.Items.RemoveAt(0);
+            return x;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if (stackList.Items.Count > 0)
+                PopItemFromStack();
+            tbInput.Text = "";
+        }
     }
 }
